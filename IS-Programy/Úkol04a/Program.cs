6f86764ddf93db34b930d0577235f9222d3ea1a3
Linda Kastlová <linda.kastlova@tul.cz>
2025-10-13 19:46:18 +0200

    odevzdat ukol#4

--- IS-Programy/Úkol04a/Program.cs
+++ IS-Programy/Úkol04a/Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************");
+    Console.WriteLine("********* Součet cifer ********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine("******** Linda Kastlová *******");
+    Console.WriteLine("********** 13.10.2025 *********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine();
 
-Console.WriteLine("Hello, World!");
+
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+
+    int input;
+    while (!int.TryParse(Console.ReadLine(), out input))
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+
+    var temp = input;
+    var result = 0;
+    var numberCount = 0;
+
+    while (temp > 0)
+    {
+        var part = temp % 10;
+        var resultSave = result;
+
+        temp /= 10;
+        numberCount++;
+        result *= part;
+
+        Console.WriteLine(
+            $"Při interaci {numberCount} jsem k číslu {resultSave} vynásobil {part} a to dělá výsledek {result} a zbývá  {temp} ke zpracování");
+    }
+
+    Console.WriteLine($"Součin všechn {numberCount} číslic ze základu {input} je {result}!");
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
--- IS-Programy/Úkol04b/Program.cs
+++ IS-Programy/Úkol04b/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************");
+    Console.WriteLine("********* Součet cifer ********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine("******** Linda Kastlová *******");
+    Console.WriteLine("********** 13.10.2025 *********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine();
 
-Console.WriteLine("Hello, World!");
+
+    Console.Write("Zadejte první číslo (celé číslo): ");
+
+    int input;
+    while (!int.TryParse(Console.ReadLine(), out input))
+        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+
+    Console.Write("Chte soušin [0] nebo součet [1]?: ");
+
+    int sum;
+    while (!int.TryParse(Console.ReadLine(), out sum) || (sum != 0 && sum != 1))
+        Console.Write("Nezadali jste 0 nebo 1. Chte soušin [0] nebo součet [1]?: ");
+
+    Console.Clear();
+
+    var inlineNumbers = input.ToString().ToList();
+
+    var result = sum == 0 ? 1 : 0;
+
+    foreach (var stringNumber in inlineNumbers)
+    {
+        var number = int.Parse(stringNumber.ToString());
+
+        Console.WriteLine($"Příčítám k {result} číslo {number}...");
+
+        if (sum == 0)
+            result *= number;
+        else
+            result += number;
+    }
+
+    var prefix = sum == 0 ? "Součin" : "Součet";
+    Console.WriteLine($"{prefix} všech {inlineNumbers.Count} číslic ze základu {input} je {result}!");
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
