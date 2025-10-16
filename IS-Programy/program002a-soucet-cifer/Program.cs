1a05a77f28d85011f1b8aac77b0c6d5a6919bb41
Linda Kastlová <linda.kastlova@tul.cz>
2025-10-16 17:08:04 +0200

    #4 Základní kód

--- IS-Programy/program002a-soucet-cifer/Program.cs
+++ IS-Programy/program002a-soucet-cifer/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************");
+    Console.WriteLine("****** Výpis číselné řady *****");
+    Console.WriteLine("*******************************");
+    Console.WriteLine("******** Linda Kastlová *******");
+    Console.WriteLine("********** 02.10.2025 *********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine();
 
-Console.WriteLine("Hello, World!");
+    // Vstup číselné hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    {
+        // Vstup číselné hodnoty do programu - lépe řešené
+        Console.Write("Zadejte první číslo řady (celé číslo): ");
+    }
+    
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+    }
+    
+            
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
