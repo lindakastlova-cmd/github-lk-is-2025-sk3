005fb9860af6d8e6781494972dd17734b6a09b81
Linda Kastlová <linda.kastlova@tul.cz>
2025-10-16 16:19:34 +0200

    Nahrát základní kód #3

--- IS-Programy/Úkol03/Program.cs
+++ IS-Programy/Úkol03/Program.cs
@@ -22,25 +22,9 @@ while (again == "a")
     while (!int.TryParse(Console.ReadLine(), out input))
         Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
 
-    var temp = input;
-    var result = 0;
-    var numberCount = 0;
-
-    while (temp > 0)
-    {
-        var part = temp % 10;
-        var resultSave = result;
-
-        temp /= 10;
-        numberCount++;
-        result += part;
-
-        Console.WriteLine(
-            $"Při interaci {numberCount} jsem k číslu {resultSave} přičetl {part} a to dělá výsledek {result} a zbývá  {temp} ke zpracování");
-    }
-
-    Console.WriteLine($"Součet všechn {numberCount} číslic ze základu {input} je {result}!");
-
+    
+    
+    
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
     again = Console.ReadLine();
