c97af9e01fe4d3e4803cd4330135c8d557592e92
Linda Kastlová <linda.kastlova@tul.cz>
2025-10-16 16:21:18 +0200

    Revert "Nahrát Program Základní kod"
    This reverts commit 8f1568108da74371ace8cba2a197f8d6f2b67032.

--- IS-Programy/Základní kód/Program.cs
+++ IS-Programy/Základní kód/Program.cs
@@ -1,31 +1,3 @@
-﻿var again = "a";
-while (again == "a")
-{
-    Console.Clear();
-    Console.WriteLine("*******************************");
-    Console.WriteLine("****** Výpis číselné řady *****");
-    Console.WriteLine("*******************************");
-    Console.WriteLine("******** Linda Kastlová *******");
-    Console.WriteLine("********** 02.10.2025 *********");
-    Console.WriteLine("*******************************");
-    Console.WriteLine();
+﻿// See https://aka.ms/new-console-template for more information
 
-    // Vstup číselné hodnoty do programu - špatně řešený
-    //Console.Write("Zadejte první číslo řady: ");
-    //int first = int.Parse(Console.ReadLine());
-
-
-    // Vstup číselné hodnoty do programu - lépe řešené
-    Console.Write("Zadejte první číslo řady (celé číslo): ");
-
-    int input;
-    while (!int.TryParse(Console.ReadLine(), out input))
-        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
-
-    
-    
-    
-    Console.WriteLine();
-    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
-    again = Console.ReadLine();
-}
+Console.WriteLine("Hello, World!");
