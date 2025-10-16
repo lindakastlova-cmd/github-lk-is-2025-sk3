8f1568108da74371ace8cba2a197f8d6f2b67032
Linda Kastlová <linda.kastlova@tul.cz>
2025-10-16 16:20:48 +0200

    Nahrát Program Základní kod

--- IS-Programy/Základní kód/Program.cs
+++ IS-Programy/Základní kód/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************");
+    Console.WriteLine("****** Výpis číselné řady *****");
+    Console.WriteLine("*******************************");
+    Console.WriteLine("******** Linda Kastlová *******");
+    Console.WriteLine("********** 02.10.2025 *********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine();
 
-Console.WriteLine("Hello, World!");
+    // Vstup číselné hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+
+    // Vstup číselné hodnoty do programu - lépe řešené
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+
+    int input;
+    while (!int.TryParse(Console.ReadLine(), out input))
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+
+    
+    
+    
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
