aaeb244893caa1e0110c856f63c2455b040c1b75
Linda Kastlová <linda.kastlova@tul.cz>
2025-10-23 17:13:55 +0200

    #6 založení projektu004a

---
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -0,0 +1,36 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************");
+    Console.WriteLine("**** Vykreslení obdélníku *****");
+    Console.WriteLine("*******************************");
+    Console.WriteLine("******** Linda Kastlová *******");
+    Console.WriteLine("********** 23.10.2025 *********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine();
+
+    
+    Console.Write("Zadejte celé číslo: ");
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
