ce6854dd6976b811f68d57c613631b0c81c89827
Linda Kastlová <linda.kastlova@tul.cz>
2025-10-23 17:35:13 +0200

    #6 Dokončení úkolu 004a

--- IS-Programy/program004a-obdelnik/Program.cs
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -11,25 +11,36 @@ while (again == "a")
     Console.WriteLine();
 
     
-    Console.Write("Zadejte celé číslo: ");
-    int number;
-    while (!int.TryParse(Console.ReadLine(), out number))
+    Console.Write("Zadejte šířku obdélníku (celé číslo): ");
+    int width;
+    while (!int.TryParse(Console.ReadLine(), out width))
     {
-        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+        Console.Write("Nezadali jste celé číslo. Zadejte šířku odbélníku znovu: ");
     }
 
+    Console.Write("Zadejte výšku obdélníku (celé číslo): ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte výšku odbélníku znovu: ");
+    }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    for(int i = 1; i <= height; i++) {
+        for (int j = 1; j <= width; j++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+        }
+        Console.WriteLine();
+    }
+
+
+
+
+
+
+
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
     again = Console.ReadLine();
