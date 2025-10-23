9544a62d905dd2e28d74642c2ffa784e64d6df7c
Linda Kastlová <linda.kastlova@tul.cz>
2025-10-23 13:01:22 +0200

    #3 základní kód

---
+++ Základní kod/Program.cs
@@ -0,0 +1,37 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************");
+    Console.WriteLine("**** Součet a součin cifer ****");
+    Console.WriteLine("*******************************");
+    Console.WriteLine("******** Linda Kastlová *******");
+    Console.WriteLine("********** 16.10.2025 *********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine();
+
+    
+    // Vstup číselné hodnoty do programu 
+    Console.Write("Zadejte celé číslo: ");
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
