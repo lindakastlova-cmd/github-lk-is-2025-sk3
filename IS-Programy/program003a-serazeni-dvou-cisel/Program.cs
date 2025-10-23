4c22c00f1eb1b4716088aec1c1e26a37175f7113
Linda Kastlová <linda.kastlova@tul.cz>
2025-10-23 16:48:21 +0200

    #5 dokončení programu 003a a založení programu003b

---
+++ IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
@@ -0,0 +1,51 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************");
+    Console.WriteLine("***** Seřazení dvou čísel *****");
+    Console.WriteLine("*******************************");
+    Console.WriteLine("******** Linda Kastlová *******");
+    Console.WriteLine("********** 23.10.2025 *********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine();
+
+    
+    Console.Write("Zadejte celé číslo A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo A znovu: ");
+    }
+
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo B znovu: ");
+    } 
+    
+    Console.WriteLine();
+    int pom;
+    if (a > b)
+    {
+        pom = a;
+        a = b;
+        b = pom;
+        Console.WriteLine("Došlo k prohození proměnných ...");
+    }
+    
+    Console.WriteLine("================================");
+    Console.WriteLine($"Seřazená čísla: {a}, {b}");
+    Console.WriteLine("================================");
+    
+    
+    
+    
+    
+    
+    
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
