a336a796c7a9dd24f6145629364d8d8cc8c1bff6
Linda Kastlová <linda.kastlova@tul.cz>
2025-11-27 17:16:02 +0100

    #11 Převod z desítkové do dvojkové soustavy - potřeba ošetřit neočekávanou výjimku

---
+++ IS-Programy/program009a-10to2/Program.cs
@@ -0,0 +1,49 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("***********************************************************");
+    Console.WriteLine("**** Převod z desítkové do binární (dvojkové) soustavy ****");
+    Console.WriteLine("***********************************************************");
+    Console.WriteLine("********************** Linda Kastlová *********************");
+    Console.WriteLine("************************ 27.11.2025 ***********************");
+    Console.WriteLine("***********************************************************");
+    Console.WriteLine();
+
+    
+    // Vstup číselné hodnoty do programu 
+    Console.Write("Zadejte přirozené číslo v desítkové soustavě: ");
+    uint number10;
+    while (!uint.TryParse(Console.ReadLine(), out number10))
+    {
+        Console.Write("Nezadali jste přirozené číslo. Zadejte přirozené číslo v desítkové soustavě znovu: ");
+    }
+
+    uint[] mojePole = new uint[32];
+    uint zálohaNumber10 = number10;
+
+    uint zbytek;
+
+    uint i;
+    for (i = 0; number10 > 0 ; i++)
+    {
+        zbytek = number10 % 2;
+        number10 = (number10 - zbytek) / 2;
+        mojePole[i] = zbytek;
+        
+        Console.WriteLine("Celá část = {0}; zbytek = {1}", number10, zbytek);
+    }
+   
+    // Zpětný výpis pole
+    Console.WriteLine("Desítkové číslo {0} ve dvojkové soustavě = ", zálohaNumber10);
+    for (uint j = i - 1; j >= 0; j--)
+    {
+       Console.Write("{0}", mojePole[j]); 
+    }
+    
+    
+    
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
